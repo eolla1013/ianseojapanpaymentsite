a10338590fd07be233c6b4e7da21cd34f21ab803
Yoshinori Yamanouchi <yamanouchi@kuh.kumamoto-u.ac.jp>
2020-06-24 16:51:01 +0900

    初期リリース

---
+++ entrypayment/appenv.js
@@ -0,0 +1,5 @@
+﻿//大会参加費支払い環境別定義ファイル
+
+const STRIPE_API_KEY = 'pk_test_51Gw1f6IsMv6XkYXjzzZiwQJPzE47pEqpKEetuaD9DqBEsOtbtJI8jXkIDka83M8sy1Ewt5mrPekuIuP36BVv2RK100tAuBLAZ4';
+const STRIPE_PRICE_KEY = 'price_1Gx8WfIsMv6XkYXjlxoY2m8u';
+const SITE_BASE_URL = 'http://localhost:65452/';
