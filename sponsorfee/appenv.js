bb712dfb6e535d6396546c672a879d287e07dee8
yamanouchi <eolla1013@gmail.com>
2020-07-15 22:24:12 +0900

    大会参加費を支払わないように変更。協賛金支払いを追加。

---
+++ sponsorfee/appenv.js
@@ -0,0 +1,5 @@
+﻿//スポンサー料金支払い環境別定義ファイル
+
+const STRIPE_API_KEY = 'pk_test_51Gw1f6IsMv6XkYXjzzZiwQJPzE47pEqpKEetuaD9DqBEsOtbtJI8jXkIDka83M8sy1Ewt5mrPekuIuP36BVv2RK100tAuBLAZ4';
+const STRIPE_PRICE_KEY = 'price_1H5AYAIsMv6XkYXj2AGE3Qpa';
+const SITE_BASE_URL = 'http://localhost:51154/';
