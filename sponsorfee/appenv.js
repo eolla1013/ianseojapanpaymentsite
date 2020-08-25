557f512fdd817a1ff325b0dc7e8b9ad75b595214
Yoshinori Yamanouchi <yamanouchi@kuh.kumamoto-u.ac.jp>
2020-08-25 17:04:07 +0900

    第2回全国小中学生リモートアーチェリー大会の協賛金登録に変更。

--- sponsorfee/appenv.js
+++ sponsorfee/appenv.js
@@ -1,5 +1,5 @@
 ﻿//スポンサー料金支払い環境別定義ファイル
 
 const STRIPE_API_KEY = 'pk_test_51Gw1f6IsMv6XkYXjzzZiwQJPzE47pEqpKEetuaD9DqBEsOtbtJI8jXkIDka83M8sy1Ewt5mrPekuIuP36BVv2RK100tAuBLAZ4';
-const STRIPE_PRICE_KEY = 'price_1H5AYAIsMv6XkYXj2AGE3Qpa';
+const STRIPE_PRICE_KEY = 'price_1HJx4iIsMv6XkYXj85xiJGbI';
 const SITE_BASE_URL = 'http://localhost:51154/';
